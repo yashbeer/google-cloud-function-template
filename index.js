36999de885d89724af307b004cfac131ada69937
sskanishk <kanishkmalviya78@gmail.com>
2022-03-17 15:27:49 +0530

    added routing support

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ exports.main = async(req, res) => {
     middleware(req, res, index); // middleware --> back to index function
 };
 
-function index(req, res) {
-    try {
-    	// write your application logic here
-    	console.log("Inside index")
-    	return res.status(200).json({ message:'Congratulations! it worked' });
-		
-	}
-	catch(err) {
-		console.log('error===>', err)
-		return res.status(500).json({ error:{code:500, message:'Something went wrong while parsing the request'} });
+function index(req, res){
+	
+	//routes
+	switch(req.url.split('/')[1])
+	{
+		// https://google-cloud-function.com/hello
+		case 'hello':
+		{
+			functions.hello(req, res);
+			break;
+		}
+		default: 
+		{
+			functions.default(req, res);
+		}
 	}
 };
